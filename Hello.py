e17e8ff5cd8d7ba55d91e696aab7b6e6abe3bdc4
jeffreychhay <jeffchhay@yahoo.com>
2017-12-14 21:46:16 -0800

    Updated to Python 3 and added proper comment style

--- Hello.py
+++ Hello.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python
+"""Hello.py: Enter name and print greeting"""
+
+__author__ = "Jeffrey Chhay"
+__copyright__ = "Copyright 2017, Hello Test"
+__credits__ = ["Jeffrey Chhay"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Jeffrey Chhay"
+__email__ = "n/a"
+__status__ = "Prototype"  # Prototype/Development/Production
+
 
 class Hello:
-    """Enter name and print greeting"""
 
     def __init__(self, name):
+        """Sets user's name"""
         self.name = name
 
     def display_name(self):
-        print "Hello " + self.name + ", and good bye."
+        """Prints greeting with user's name"""
+        print("Hello " + self.name + ", and good bye.")
 
 
-"Take user input as string"
-action = raw_input("Enter name: ")
+# Take user input as string
+action = input("Enter name: ")
 
-"Create object for Hello Class"
+# Create object for Hello Class
 hello = Hello(action)
 
 hello.display_name()
